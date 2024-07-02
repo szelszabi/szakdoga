73a2cad2d3f7cfbd016aa868f4c9f487230c62c8
Szabolcs Szél <szel.szabolcs02@gmail.com>
2024-07-02 12:49:59 +0200

    Memory access fixed

--- src/masik.py
+++ src/masik.py
@@ -10,44 +10,45 @@ const_counter = 0
 mem_write = 0
 mem_read = 0
 
-def hook_mem_write(uc, access, address, size, value, user_data):
-    global mem_write
-    mem_write += 1
-
-def hook_mem_read(uc, access, address, size, value, user_data):
-    global mem_read
-    mem_read += 1
-
 def hook_code(uc, address, size, user_data):
-    global const_counter
+    global const_counter, mem_write, mem_read
     md = capstone.Cs(capstone.CS_ARCH_X86, capstone.CS_MODE_64)
     md.detail = True
     inst_bytes = uc.mem_read(address,size)
     for i in md.disasm(inst_bytes, address):
 
-        for operand in i.operands:
-            if operand.type == cx86.X86_OP_IMM:
-                print("\t\tIMM = 0x%x" %(operand.value.imm))
-            if operand.type == cx86.X86_OP_MEM:
-                print("\t\ttype: MEM")
-                if operand.value.mem.base != 0:
-                    print("\t\t\tmem.base: REG = %s" \
-                        %(i.reg_name(operand.value.mem.base)))
-                if operand.value.mem.index != 0:
-                    print("\t\t\tmem.index: REG = %s" \
-                        %(i.reg_name(operand.value.mem.index)))
-                if operand.value.mem.disp != 0: # offset
-                    print("\t\t\tmem.disp: 0x%x" \
-                        %(operand.value.mem.disp))
-                if operand.value.mem.scale != 0:
-                    print(f"\t\t\tscale factor: {operand.value.mem.scale}")
-                
-            if operand.type == cx86.X86_OP_REG:
-                print("\t\t.type: REG = %s" %(i.reg_name(operand.value.reg)))
-
-        for operand in i.op_str.split(','):
-            if "0x" in operand:
+        for op in i.operands:
+            if op.type == cx86.X86_OP_IMM:
+                print("\t\tIMM = 0x%x" %(op.value.imm))
                 const_counter += 1
+
+
+            if op.type == cx86.X86_OP_MEM:
+                print("\t\ttype: MEM")
+                if op.mem.base != 0:
+                    print("\t\t\tmem.base: REG = %s" \
+                        %(i.reg_name(op.mem.base)))
+                if op.mem.index != 0:
+                    print("\t\t\tmem.index: REG = %s" \
+                        %(i.reg_name(op.mem.index)))
+                if op.mem.disp != 0: # offset
+                    print("\t\t\tmem.disp: 0x%x" \
+                        %(op.mem.disp))
+                if op.mem.scale != 0:
+                    print(f"\t\t\tscale factor: {op.mem.scale}")
+
+                match(op.access):
+                    case capstone.CS_AC_READ:
+                        mem_read += 1
+                    case capstone.CS_AC_WRITE:
+                        mem_write += 1
+
+
+            if op.type == cx86.X86_OP_REG:
+                pass
+                print("\t\t.type: REG = %s" %(i.reg_name(op.value.reg)))
+
+            
 
         key = f"{hex(i.address)}    {i.mnemonic}"
         if  key not in list(inst_counter.keys()):
@@ -63,6 +64,7 @@ def hook_code(uc, address, size, user_data):
 data, main_addr = asm_to_main_machine_code('a.out')
 
 # X86_CODE32 = b"\x66\xbb\x01\x00\x66\xb8\x01\x00\xeb\x08\x66\xf7\xe3\x66\x99\x66\xff\xc3\x66\x83\xfb\x05\x7e\xf2\x66\x89\xc1"
+INIT_CODE = "asd" # TODO putting a special value on top of stack to check final return
 X86_CODE32 = bytes(data)
 ADDRESS = 0x0
 
@@ -74,8 +76,6 @@ try:
     mu.reg_write(UC_X86_REG_RSP, ADDRESS + 0x200000)
 
     mu.hook_add(UC_HOOK_CODE, hook_code)
-    mu.hook_add(UC_HOOK_MEM_WRITE, hook_mem_write)
-    mu.hook_add(UC_HOOK_MEM_READ, hook_mem_read)
 
     mu.emu_start(ADDRESS + main_addr, ADDRESS + len(X86_CODE32))
 
@@ -89,3 +89,5 @@ except UcError as e:
 print(json.dumps(inst_counter,sort_keys=True, indent=4))
 print(f"Konstansok száma: {const_counter}")
 print(f"Memóriahozzáférések száma: {mem_write + mem_read}")
+print(f"Memória írás: {mem_write}")
+print(f"Memória olvas: {mem_read}")
